b7ca52598debc0de1eb6f5b8dca74acb4db6a59c
Artur Ratajczak <artur.ratajczak1@gmail.com>
2015-05-31 23:19:22 +0200

    Test pusha na branch testers.

---
+++ src/piatek/jacek/TestCore.java
@@ -0,0 +1,14 @@
+package piatek.jacek;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCore {
+
+	@Test
+	public void testMin() {
+		fail("Nic tutaj nie ma! Tylko na test gita robiê. AR");
+	}
+
+}
